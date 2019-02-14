24f75b0cc82ebdb190bcdc97c09c7a8b86ea5ad1
LLS <2247407869@qq.com>
2019-02-14 20:00:30 +0800

    update

--- acgshelf-service-admin/src/main/java/wang/acgshelf/acgshelf/service/admin/AcgshelfServiceAdminApplication.java
+++ acgshelf-service-admin/src/main/java/wang/acgshelf/acgshelf/service/admin/AcgshelfServiceAdminApplication.java
@@ -2,8 +2,10 @@ package wang.acgshelf.acgshelf.service.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AcgshelfServiceAdminApplication {
 
     public static void main(String[] args) {
