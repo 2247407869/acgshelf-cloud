c2f95cc32b24ac52802a83c344672a2289496ee5
linsong.li <linsong.li@teradata.com>
2019-02-14 16:14:42 +0800

    update

---
+++ acgshelf-service-admin/src/main/java/wang/acgshelf/acgshelf/service/admin/AcgshelfServiceAdminApplication.java
@@ -0,0 +1,14 @@
+package wang.acgshelf.acgshelf.service.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcgshelfServiceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcgshelfServiceAdminApplication.class, args);
+    }
+
+}
+
