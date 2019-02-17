55c73fbafa998a56665c89b5ef811c5fe20726f9
LLS <2247407869@qq.com>
2019-02-17 10:18:02 +0800

    创建service-redis

---
+++ acgshelf-service-redis/src/main/java/wang/acgshelf/acgshelf/service/redis/AcgshelfServiceRedisApplication.java
@@ -0,0 +1,13 @@
+package wang.acgshelf.acgshelf.service.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcgshelfServiceRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcgshelfServiceRedisApplication.class, args);
+    }
+
+}
